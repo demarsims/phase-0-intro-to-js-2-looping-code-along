68ca7c6bad940a12703ea0f123af4e4814706f24
Demarcus Sims <d.sims1993@yahoo.com>
2023-07-26 12:32:16 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names){
+    const thankyoumessages = []
+    for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    thankyoumessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return thankyoumessages
+}
+    
+function countDown(number) {
+    while (number>-1) {
+        console.log(number--)
+    }
+}
